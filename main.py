248dfd17944197e8caacdb2e6df0b509bdee7e12
= <me@bryceyoder.com>
2021-11-19 18:21:39 -0500

    Default to instruct engine and remove 'Doug:' from responses

--- main.py
+++ main.py
@@ -14,14 +14,35 @@ openai.api_key = env_vars["OPENAI_KEY"]
 app = App(token=BOT_TOKEN)
 
 prompt = """
-Background:
-I am Doug.
-Moodle is an LMS (learning management system).
-Moodle is the most popular open-source LMS in the world.
-We work for Moodle.
-I created Moodle.
+The following is a conversation with Doug, the creator of Moodle. Moodle is an LMS (learning management system), the most popular open-source LMS in the world. Doug is having a conversation with another employee who also works for Moodle.
 
-"""
+Human: Hi Doug, how are you?
+Doug: I'm doing okay. What's up?
+Human: """
+
+
+def process_query(query):
+    engine = 'davinci-instruct-beta'
+    split_query = query.split('|')
+    if (len(split_query) > 1) and (split_query[1].strip() == 'davinci'):
+        engine = 'davinci'
+
+
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt + split_query[0] + '\nDoug: ',
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=["Human:"]
+    )
+
+    text_response = response['choices'][0]['text']
+    text_response = ' '.join(text_response.split('Doug:'))
+    print(prompt + split_query[0] + "\nDoug: " + text_response)
+    return text_response
 
 
 @app.event("app_mention")
@@ -32,19 +53,12 @@ def mention_handler(body, say):
 
 @app.event("message")
 def message_handler(body, say):
+    response = process_query(body['event']['text'])
     print(body)
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt + "\nQuestion: " + body['event']['text'] + "\nPolite response: ",
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[".", "!"]
-    )
-    say(response['choices'][0]['text'])
+    say(response)
+
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, APP_TOKEN)
     handler.start()
+
