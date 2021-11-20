ffc2d50f615bf823071ff199aa31c0ed55ea2784
= <me@bryceyoder.com>
2021-11-19 23:45:23 -0500

    Doug can be tagged and asked to summarize messages in a channel

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ with open('.env') as f:
 
 APP_TOKEN = env_vars["APP_TOKEN"]
 BOT_TOKEN = env_vars["BOT_TOKEN"]
+BOT_ID = env_vars["BOT_ID"]
 openai.api_key = env_vars["OPENAI_KEY"]
+
 app = App(token=BOT_TOKEN)
 
 prompt = """
@@ -21,16 +23,15 @@ Doug: I'm doing okay. What's up?
 Human: """
 
 
-def process_query(query):
+def process_query(query, query_prompt=prompt):
     engine = 'davinci-instruct-beta'
     split_query = query.split('|')
     if (len(split_query) > 1) and (split_query[1].strip() == 'davinci'):
         engine = 'davinci'
 
-
     response = openai.Completion.create(
         engine=engine,
-        prompt=prompt + split_query[0] + '\nDoug: ',
+        prompt=query_prompt + split_query[0] + '\nDoug: ',
         temperature=0.9,
         max_tokens=150,
         top_p=1,
@@ -45,16 +46,40 @@ def process_query(query):
     return text_response
 
 
+def format_conversation_prompt(convo_data):
+    real_prompt = prompt
+    for index, message in enumerate(convo_data[::-1]):
+        user = 'Human: '
+        if 'bot_id' in message:
+            user = 'Doug: '
+        if index == 0:
+            real_prompt += message['text'] + "\n"
+        else:
+            real_prompt += user + message['text'] + "\n"
+    real_prompt = real_prompt.replace('<@' + BOT_ID + '>', '')
+    print('real prompt:')
+    print(real_prompt)
+    real_prompt += "\nDoug: "
+    return real_prompt
+
+
 @app.event("app_mention")
 def mention_handler(body, say):
-    print(body)
-    say("got it")
+    response = None
+    query = body['event']['text'].replace('<@' + BOT_ID + '>', '')
+    if 'what do you think' in query.lower():
+        channel_id = body['event']['channel']
+        convo_data = app.client.conversations_history(channel=channel_id, limit=10)
+        real_prompt = format_conversation_prompt(convo_data['messages'])
+        response = process_query(query, query_prompt=real_prompt)
+    else:
+        response = process_query(query)
+    say(response)
 
 
 @app.event("message")
 def message_handler(body, say):
     response = process_query(body['event']['text'])
-    print(body)
     say(response)
 
 
