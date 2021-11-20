b0320f6701a5bf4f3d3a6393df1e8e65ba7315ee
= <me@bryceyoder.com>
2021-11-19 23:56:46 -0500

    Add comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import openai
 
+# Read vars from .env
 env_vars = {}
 with open('.env') as f:
     for line in f:
@@ -24,6 +25,12 @@ Human: """
 
 
 def process_query(query, query_prompt=prompt):
+    """
+    Takes a query string (usually a question) and optionally a prompt string, passes it to OpenAI, and returns a response
+    If unspecified, uses the global prompt variable
+    """
+    # Allow the user to determine the engine to use
+    # ie, "What is your favorite animal | davinci" will use the davinci engine; defaults to davinci-instruct-beta
     engine = 'davinci-instruct-beta'
     split_query = query.split('|')
     if (len(split_query) > 1) and (split_query[1].strip() == 'davinci'):
@@ -41,12 +48,19 @@ def process_query(query, query_prompt=prompt):
     )
 
     text_response = response['choices'][0]['text']
-    text_response = ' '.join(text_response.split('Doug:'))
+    text_response = ' '.join(text_response.split('Doug:')) # We don't want the bot to actually SAY "Doug:" in the response
     print(prompt + split_query[0] + "\nDoug: " + text_response)
+
+    if text_response == '':
+        text_response = ':)'
+
     return text_response
 
 
 def format_conversation_prompt(convo_data):
+    """
+    Takes a Slack conversation object from conversations.history and formats it into/returns a prompt string to be ingested by OpenAI
+    """
     real_prompt = prompt
     for index, message in enumerate(convo_data[::-1]):
         user = 'Human: '
@@ -56,7 +70,7 @@ def format_conversation_prompt(convo_data):
             real_prompt += message['text'] + "\n"
         else:
             real_prompt += user + message['text'] + "\n"
-    real_prompt = real_prompt.replace('<@' + BOT_ID + '>', '')
+    real_prompt = real_prompt.replace('<@' + BOT_ID + '>', '') # Remove internal Slack ID
     print('real prompt:')
     print(real_prompt)
     real_prompt += "\nDoug: "
@@ -65,8 +79,13 @@ def format_conversation_prompt(convo_data):
 
 @app.event("app_mention")
 def mention_handler(body, say):
+    """
+    When the bot is mentioned, process a response
+    """
     response = None
     query = body['event']['text'].replace('<@' + BOT_ID + '>', '')
+    # If the string contains "what do you think," Doug will summarize the last 10 messages of the current conversation
+    # Otherwise, only respond to the message he was tagged in
     if 'what do you think' in query.lower():
         channel_id = body['event']['channel']
         convo_data = app.client.conversations_history(channel=channel_id, limit=10)
@@ -79,6 +98,9 @@ def mention_handler(body, say):
 
 @app.event("message")
 def message_handler(body, say):
+    """
+    When the bot receives a DM, respond to it
+    """
     response = process_query(body['event']['text'])
     say(response)
 
