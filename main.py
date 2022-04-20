1f927c4b077415678271a8d2c57e6d25bbf19530
Limekiller <me@bryceyoder.com>
2022-04-20 16:43:06 +0000

    Allow Doug to maintain conversation context on Slack

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import openai
 import string
+import time
 
 # Read vars from .env
 env_vars = {}
-with open('.env') as f:
+with open('.env-test') as f:
     for line in f:
         key, value = line.strip().split('=', 1)
         env_vars[key] = value
@@ -23,24 +24,32 @@ Human: Hi Doug. I'm also an employee at Moodle. Can I ask you a question?
 Doug: Absolutely, what's your question? I can give you a quick, polite answer.
 Human: """
 
+engines = [
+    'text-davinci-002',
+    'text-curie-001',
+    'text-babbage-001',
+    'text-ada-001'
+]
 
-def process_query(query, query_prompt=prompt):
+conversation_dict = {'channels': {}, 'DMs': {}}
+
+def process_query(query, prompt=prompt):
     """
     Takes a query string (usually a question) and optionally a prompt string, passes it to OpenAI, and returns a response
     If unspecified, uses the global prompt variable
     """
     # Allow the user to determine the engine to use
     # ie, "What is your favorite animal | davinci" will use the davinci engine; defaults to davinci-instruct-beta
-    engine = 'davinci'
+    engine = 'text-davinci-002'
     split_query = query.split('|')
-    if (len(split_query) > 1) and (split_query[1].strip() == 'davinci-instruct-beta'):
-        engine = 'davinci-instruct-beta'
+    if (len(split_query) > 1) and (split_query[1].strip() in engines):
+        engine = split_query[1].strip()
 
     response = openai.Completion.create(
         engine=engine,
-        prompt=query_prompt + split_query[0] + '\nDoug: ',
+        prompt=prompt + split_query[0] + '\nDoug: ',
         temperature=0.9,
-        max_tokens=75,
+        max_tokens=500,
         top_p=1,
         frequency_penalty=1.0,
         presence_penalty=0.6,
@@ -50,29 +59,57 @@ def process_query(query, query_prompt=prompt):
     text_response = response['choices'][0]['text']
     text_response = ' '.join(text_response.split('Doug:')) # We don't want the bot to actually SAY "Doug:" in the response
     print('full message:')
-    print(prompt + split_query[0] + "\nDoug: " + text_response)
+    print(prompt + split_query[0] + "\nDoug: " + text_response.strip())
 
-    return text_response
+    return text_response.strip()
 
 
-def format_conversation_prompt(convo_data):
-    """
-    Takes a Slack conversation object from conversations.history and formats it into/returns a prompt string to be ingested by OpenAI
-    """
-    real_prompt = prompt
-    for index, message in enumerate(convo_data[::-1]):
-        user = 'Human: '
-        if 'bot_id' in message:
-            user = 'Doug: '
-        if index == 0:
-            real_prompt += message['text'] + "\n"
+def format_message_history_prompt(user, channel=None):
+    new_prompt = prompt
+    message_list = []
+
+    # determine which source to pull the message list from
+    if channel:
+        if channel not in conversation_dict['channels']:
+            return new_prompt
+        new_prompt = """Doug is the creator of Moodle. Moodle is an LMS (learning management system), the most popular open-source LMS in the world. The following is a transcript from a conversation he is having with multiple employees:\n"""
+        message_list = conversation_dict['channels'][channel]['messages']
+    else:
+        if user not in conversation_dict['DMs']:
+            return new_prompt
+        new_prompt = """Doug is the creator of Moodle. Moodle is an LMS (learning management system), the most popular open-source LMS in the world. The following is a transcript from a conversation he is having with an employee:\n"""
+        message_list = conversation_dict['DMs'][user]['messages']
+
+    for message in message_list:
+        user = 'Doug'
+        if message['user'] != 'Doug':
+            user = 'Employee'
+        new_prompt += user + ': ' + message['body'] + "\n"
+    new_prompt += "Employee: "
+
+    return new_prompt
+
+
+def record_conversation(user, body, channel=None):
+    if channel:
+        if channel in conversation_dict['channels']:
+            # reset convo if response time is greater than 10 minutes
+            if time.time() - conversation_dict['channels'][channel]['time'] > 600:
+                conversation_dict['channels'][channel]['messages'] = []
+
+            conversation_dict['channels'][channel]['messages'].append({'user': user, 'body': body})
+            conversation_dict['channels'][channel]['time'] = time.time()
         else:
-            real_prompt += user + message['text'] + "\n"
-    real_prompt = real_prompt.replace('<@' + BOT_ID + '>', '') # Remove internal Slack ID
-    print('real prompt:')
-    print(real_prompt)
-    real_prompt += "\nDoug: "
-    return real_prompt
+            conversation_dict['channels'][channel] = {'messages': [{'user': user, 'body': body}], 'time': time.time()}
+    else:
+        if user in conversation_dict['DMs']:
+            if time.time() - conversation_dict['DMs'][user]['time'] > 600:
+                conversation_dict['DMs'][user]['messages'] = []
+
+            conversation_dict['DMs'][user]['messages'].append({'user': user, 'body': body})
+            conversation_dict['DMs'][user]['time'] = time.time()
+        else:
+            conversation_dict['DMs'][user] = {'messages': [{'user': user, 'body': body}], 'time': time.time()}
 
 
 @app.event("app_mention")
@@ -83,15 +120,9 @@ def mention_handler(body, say):
     response = ''
     query = body['event']['text'].replace('<@' + BOT_ID + '>', '')
     channel_id = body['event']['channel']
-    # If the string contains "what do you think," Doug will summarize the last 10 messages of the current conversation
-    # Otherwise, only respond to the message he was tagged in
-    while response.translate(str.maketrans('', '', string.punctuation)).strip() == '':
-        if 'what do you think' in query.lower():
-            convo_data = app.client.conversations_history(channel=channel_id, limit=10)
-            real_prompt = format_conversation_prompt(convo_data['messages'])
-            response = process_query(query, query_prompt=real_prompt)
-        else:
-            response = process_query(query)
+
+    ai_prompt = format_message_history_prompt(body['event']['user'], channel_id)
+    response = process_query(query, ai_prompt)
 
     # If the message is part of a thread, respond in that thread
     if 'thread_ts' in body['event']:
@@ -100,7 +131,12 @@ def mention_handler(body, say):
             thread_ts = body['event']['thread_ts'],
             text = response
         )
+
     else:
+        record_conversation(body['event']['user'], query, channel_id)
+        conversation_dict['channels'][channel_id]['messages'].append({'user': 'Doug', 'body': response})
+        print(conversation_dict)
+
         say(response)
 
 
@@ -110,8 +146,13 @@ def message_handler(body, say):
     When the bot receives a DM, respond to it
     """
     response = ''
-    while response.translate(str.maketrans('', '', string.punctuation)).strip() == '':
-        response = process_query(body['event']['text'])
+    ai_prompt = format_message_history_prompt(body['event']['user'])
+    response = process_query(body['event']['text'], ai_prompt)
+
+    record_conversation(body['event']['user'], body['event']['text'])
+    conversation_dict['DMs'][body['event']['user']]['messages'].append({'user': 'Doug', 'body': response})
+    print(conversation_dict)
+
     say(response)
 
 
